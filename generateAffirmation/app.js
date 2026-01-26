84e7d340b971ac2ee1a3d1dea8c4bd83f0d584ca
Shri-coder413 <shrinivasnakadi@gmail.com>
2026-01-26 20:31:54 +0530

    updated logic of background image

--- generateAffirmation/app.js
+++ generateAffirmation/app.js
@@ -8,6 +8,10 @@ const affirmations = [
     bg: "https://i.pinimg.com/736x/f3/5e/aa/f35eaa7fc7e695448eefed14b8abbe16.jpg",
   },
   {
+    text: "I am consistent, and my confidence grows every day",
+    bg: "https://i.pinimg.com/736x/25/3f/7e/253f7e8ab1ded253e0b7f24e2a83b9be.jpg",
+  },
+  {
     text: "I am progressing every day, not perfect but moving forward",
     bg: "https://i.pinimg.com/736x/a6/e8/c3/a6e8c32546ae4a5c6417164fdac0af2a.jpg",
   },
@@ -15,29 +19,30 @@ const affirmations = [
     text: "I am calm, capable, and confident in myself",
     bg: "https://i.pinimg.com/1200x/a7/49/58/a749587d9ff8cefe6b24ddb574a251b7.jpg",
   },
-  {
-    text: "I am consistent, and my confidence grows every day",
-    bg: "hhttps://i.pinimg.com/736x/e4/ef/06/e4ef0618a23259206d0229aa7dcfba2b.jpg",
-  },
 ];
 
-// 2. Get HTML elements
 const affirmationEl = document.getElementById("affirmation");
 const generateBtn = document.getElementById("generateBtn");
 
-function showAffirmation() {
+let bgIndex = 0; // controls image order
 
-  const randomIndex = Math.floor(Math.random() * affirmations.length);
-  const current = affirmations[randomIndex];
+function showAffirmation() {
+  const randomText = affirmations[Math.floor(Math.random() * affirmations.length)].text;
+
+  const currentBg = affirmations[bgIndex].bg;
 
   affirmationEl.style.opacity = 0;
 
-  setTimeout(() => {
-    affirmationEl.textContent = current.text;
-    document.body.style.backgroundImage = `url(${current.bg})`;
+  const img = new Image();
+  img.src = currentBg;
 
+  img.onload = () => {
+    document.body.style.backgroundImage = `url(${currentBg})`;
+    affirmationEl.textContent = randomText;
     affirmationEl.style.opacity = 1;
-  }, 200);
+  };
+
+  bgIndex = (bgIndex + 1) % affirmations.length;
 }
 
 generateBtn.addEventListener("click", showAffirmation);
